40f7df17e827bdddafc49da80d01dc17543ce417
PatrickC96 <patrick.cabezas@epn.edu.ec>
2020-07-14 08:58:32 -0500

    pandas

---
+++ 03-pandas/a_introduccion.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Editor de Spyder
+
+Este es un archivo temporal.
+"""
+print("hola spider")
+
+nombre="Patrick"
+numero=3
+lista=[1,2,3]
+tupla=(4,5,6)
+diccionario={
+    "nombre":"David",
+    "apellido":"Cabezas"
+    }
